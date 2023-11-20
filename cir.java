a97680b458dfdb3a73df179977f647309bcbb0a2
shubhambharambe <shubhambharambe15@gmail.com>
2023-11-20 19:00:42 +0530

    logreg.html

---
+++ cir.java
@@ -0,0 +1,18 @@
+package pg;
+public class cir implements G {
+    private double radius;
+
+    public cir(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ hello.java
@@ -0,0 +1,6 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app \n by using Docker");  
+    System.out.println("this code is use for jenkins:");
+    }  
+    }  
